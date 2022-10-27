dd8944015ac3418a5fa42e3d4f9eec6cae3ef99d
Luigi Carvalho <102177155+carvalholuigi25@users.noreply.github.com>
2022-10-27 11:39:45 +0000

    updated keep_alive script

--- keep_alive.js
+++ keep_alive.js
@@ -1,7 +1,39 @@
 var http = require('http');
-var port = 5000;
+var createHttpTerminator = require('http-terminator');
 
-http.createServer(function (req, res) {
-  res.write("I'm alive");
-  res.end();
-}).listen(port);
+async function startServer(port, server) {
+  http.createServer(function(req, res) {
+    res.write("I'm alive");
+    res.end();
+  }, server).listen(port);
+
+  if (server != null) {
+    await stopServer(server);
+  }
+}
+
+async function stopServer(server) {
+  const httpTerminator = createHttpTerminator({
+    server,
+  });
+
+  await httpTerminator.terminate();
+}
+
+async function keepServerAlive(port, server, hour = 24, modetype = "normal") {
+  if (modetype == "timeout") {
+    clearTimeout();
+    setTimeout(() => {
+      await startServer(port, server);
+    }, 60 * 60 * hour);
+  } else if (modetype == "interval") {
+    clearInterval();
+    setInterval(() => {
+      await startServer(port, server);
+    }, 60 * 60 * hour);
+  } else {
+    await startServer(port, server);
+  }
+}
+
+module.exports = { startServer, stopServer, keepServerAlive };
